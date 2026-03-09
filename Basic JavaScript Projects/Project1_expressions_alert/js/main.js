739d9650cd9d62ffe1b913f96cc2f89c40cad21a
ivanmaker <mrisaacvazquez@gmail.com>
2026-03-09 13:32:32 -0700

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/js/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/js/main.js
@@ -3,3 +3,5 @@ var sent1 = "This is the beginning of the string", sent2 = " and this is the end
 
 /*Concatenate the two previously created variable and display them in a popup*/
 window.alert(sent1 + sent2);
+
+document.write(sent1 + sent2 + " with a bit more text added");
