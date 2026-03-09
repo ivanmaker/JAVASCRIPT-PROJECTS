6c0f417890e1f2c82c9dbfe0a40fa748e138991b
ivanmaker <mrisaacvazquez@gmail.com>
2026-03-09 13:30:35 -0700

    Created Project1_expressions_alert
    Scratching the surface of JS integration into HTML and CSS

---
+++ Basic JavaScript Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,5 @@
+/*Declare and assign two variables for later use*/
+var sent1 = "This is the beginning of the string", sent2 = " and this is the end of the string"; 
+
+/*Concatenate the two previously created variable and display them in a popup*/
+window.alert(sent1 + sent2);
