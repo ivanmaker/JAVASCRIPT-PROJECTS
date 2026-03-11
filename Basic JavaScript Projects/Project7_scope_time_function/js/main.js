6d3466808b16644572907faa2d278c0e3509ac19
ivanmaker <mrisaacvazquez@gmail.com>
2026-03-11 11:32:05 -0700

    Update main.js

--- Basic JavaScript Projects/Project7_scope_time_function/js/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/js/main.js
@@ -1,15 +1,20 @@
 
+/*Global scoped variable to used in the Log_Test function*/
 let name = "James";
 
+/*Out puts text to the console*/
 function Log_Test(){
     let text = " loves pickles!"
     let sentence = name + text;
     console.log(sentence);
 }
 Log_Test();
+
+/*Tests local variable scope to show an error in the console because the sentence variable is inaccessible outside the function it's in*/
 console.log(sentence);
 
 
+/*This function gives you a greeting based on the time of the day, uses the Date object*/
 function Greetings(){
     if (new Date().getHours() < 18){
         document.getElementById("greetings").innerHTML = "Hope you're having a good day!";
@@ -19,6 +24,7 @@ function Greetings(){
     }
 }
 
+/*Testing if/else, could have used a varibale for output text instead of hardcoding it, but this function was simple enough that it didn't seem neccessary*/
 function Teller(){
     num = document.getElementById("given-number").value;
     if (num >= 10){
@@ -29,6 +35,7 @@ function Teller(){
     }
 }
 
+/*This funciton lets you know what it is based on the hour of the day, uses Date.getHours()*/
 function Time_Function(){
     let Time = new Date().getHours();
     let Reply = "";
